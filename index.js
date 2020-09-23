bac484f053cff2bbf8d60f5f7a61bc75a43ad50e
ahsanarshad777 <70811519+ahsanarshad777@users.noreply.github.com>
2020-09-23 23:36:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,188 @@
-// sub catergory Dropdown
+// sub catergory1 Dropdown
 
 $(document).ready(function(){
-  $('.menu-item > a').on('click',function(e){
+  $('.menu-item1 > a').on('click',function(e){
     e.preventDefault();
     $(this).next('.subcategory-list1').slideToggle("slow");
+  });
+});
+
+// sub catergory2 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item2 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list2').slideToggle("slow");
+  });
+});
+
+// sub catergory3 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item3 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list3').slideToggle("slow");
+  });
+});
+
+// sub catergory4 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item4 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list4').slideToggle("slow");
+  });
+});
+
+// sub catergory5 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item5 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list5').slideToggle("slow");
+  });
+});
+
+// sub catergory6 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item6 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list6').slideToggle("slow");
+  });
+});
+
+// sub catergory7 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item7 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list7').slideToggle("slow");
+  });
+});
+
+// sub catergory8 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item8 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list8').slideToggle("slow");
+  });
+});
+
+// sub catergory9 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item9 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list9').slideToggle("slow");
+  });
+});
+
+// sub catergory10 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item10 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list10').slideToggle("slow");
+  });
+});
+
+// sub catergory11 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item11 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list11').slideToggle("slow");
+  });
+});
+
+// sub catergory12 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item12 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list12').slideToggle("slow");
+  });
+});
+
+// sub catergory13 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item13 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list13').slideToggle("slow");
+  });
+});
+
+// sub catergory14 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item14 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list14').slideToggle("slow");
+  });
+});
+
+// sub catergory15 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item15 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list15').slideToggle("slow");
+  });
+});
+
+// sub catergory16 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item16 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list16').slideToggle("slow");
+  });
+});
+
+// sub catergory17 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item17 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list17').slideToggle("slow");
+  });
+});
+
+// sub catergory18 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item18 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list18').slideToggle("slow");
+  });
+});
+
+// sub catergory19 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item19 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list19').slideToggle("slow");
+  });
+});
+
+// sub catergory20 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item20 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list20').slideToggle("slow");
+  });
+});
+
+// sub catergory21 Dropdown
+
+$(document).ready(function(){
+  $('.menu-item21 > a').on('click',function(e){
+    e.preventDefault();
+    $(this).next('.subcategory-list21').slideToggle("slow");
   });
 });
